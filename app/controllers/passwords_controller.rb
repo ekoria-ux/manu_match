950c6c9d784fb9466f8e68cdcf1b297978c5b65c
AKIRA KOIKE <ekoria@mba-ekoria.local>
2021-03-14 18:56:00 +0900

    password controller if文ネスト修正

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -11,16 +11,11 @@ class PasswordsController < ApplicationController
   def update
     @user = current_user
     current_password = params[:account][:current_password]
-    if current_password.present? && params[:account][:password].present?
-      if @user.valid_password?(current_password)
-        if @user.update(password_params)
-          redirect_to :account, flash: { success: "パスワードを変更しました" }
-        else
-          flash.now[:danger] = "変更に失敗しました"
-          render "edit"
-        end
+    if current_password.present? && params[:account][:password].present? && @user.valid_password?(current_password)
+      if @user.update(password_params)
+        redirect_to :account, flash: { success: "パスワードを変更しました" }
       else
-        @user.errors.add(:current_password, :wrong)
+        flash.now[:danger] = "変更に失敗しました"
         render "edit"
       end
     elsif current_password.blank?
@@ -28,6 +23,9 @@ class PasswordsController < ApplicationController
       render "edit"
     elsif params[:account][:password].blank?
       @user.errors.add(:password, :empty)
+      render "edit"
+    else
+      @user.errors.add(:current_password, :wrong)
       render "edit"
     end
   end
