29071458d7f1fa777749fdbaa9958d910c5fe099
Akira Koike <kaisou_wakame@yahoo.co.jp>
2021-11-09 20:14:12 +0900

    UI改善、画像プレビュー

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,11 @@ class StaticPagesController < ApplicationController
   include Pagy::Backend
 
   def home
-    @pagy, @articles = pagy(Article.includes([:author]).visible.order(created_at: :desc))
+    @pagy, @articles = pagy(Article.includes([:author]).visible.published.order(created_at: :desc))
     if params[:search]
-      @pagy, @articles = pagy(Article.search(params[:search]).visible.order(created_at: :desc))
+      @pagy, @articles = pagy(
+        Article.search(params[:search]).visible.published.order(created_at: :desc)
+      )
     end
   end
 
